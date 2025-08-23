ba2daaaabed4eab38f8cefd3a97c595b21aa5149
krishna zunj <krishnazunj.dev@gmail.com>
2025-08-23 19:12:28 +0530

    axiosChange

--- GharKaAchar/src/utils/axiosClient.js
+++ GharKaAchar/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000', // trailing slash optional
+  baseURL: 'https://gharkaachar.onrender.com', // trailing slash optional
   withCredentials: true, // ✅ ONLY here, not in headers
   headers: {
     'Content-Type': 'application/json'
